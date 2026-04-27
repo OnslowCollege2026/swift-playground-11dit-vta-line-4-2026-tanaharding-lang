353bf44adeb286fbb9d3610ef5bd67dce49fb416
tanaharding-lang <tana.harding@student.onslow.school.nz>
2026-04-27 21:39:38 +0000

    Apr 27, 2026, 9:39 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,28 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
-    }
+        //prints Hello, world!
+
+
+        print("TTTTTTT     H     H")
+        print("   T        H     H")
+        print("   T        HHHHHHH")
+        print("   T        H     H")
+        print("   T        H     H")
+        //prints initials
+
+let school = "Onslow College"
+let schoolHours = 6
+var studentName = "Zach"
+var studentAge = 15
+var studentYear = 11
+var greeting = "There is a young \(studentAge) year old boy called \(studentName). He is in year \(studentYear) at \(school). \(studentName) does \(schoolHours) hours of school work every day and then he goes home."
+print(greeting)
+
+studentName = "Mr Beast"
+studentYear = 6
+studentAge = 27
+greeting = "There is a young \(studentAge) year old boy called \(studentName). He is in year \(studentYear) at \(school). \(studentName) does \(schoolHours) hours of school work every day and then he goes home."
+print(greeting)
+}
 }
